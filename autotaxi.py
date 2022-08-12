3f52ca641a0fa431762cb1403717a9f175154012
ильназ <102890231+nrokg@users.noreply.github.com>
2022-08-12 12:08:05 +0300

    Update autotaxi.py

--- autotaxi.py
+++ autotaxi.py
@@ -1,48 +1,59 @@
 import requests
-import vk_api
 from random import randint
 import time
-# ////////////////////////////////////////////////////////////////////
+
+###################################################
 token = ""
-# ////////////////////////////////////////////////////////////////////
-vk_session = vk_api.VkApi(token=token)
-vk = vk_session.get_api()
+###################################################
 
 while 1 == 1:
     try:
         time.sleep(7)
-        g = vk.messages.send(peer_id=-166948584, random_id=randint(0,999999999999), message="я")
-        str(g).replace("	", "")
+        g = requests.get("https://api.vk.com/method/messages.send?access_token=" + token + "&peer_id=-166948584&random_id=" + str(randint(0,999999999999)) + "&message=я&v=5.131")
+        g = g.text
+        g = g.replace("{", "")
+        g = g.replace('response":', "")
+        g = g.replace("}", "")
+        g = g.replace('"', "")
         g = int(g) + 1
         time.sleep(5)
-        p = vk.messages.getById(message_ids=g)
-        p = str(p)
+        p = requests.get("https://api.vk.com/method/messages.getById?access_token=" + token + "&message_ids=" + str(g) + "&v=5.131")
+        p = p.text
         a = p.rfind("$")
         b = p.rfind(".")
         g = p[a:]
         g = g.replace(".'}]}", '')
         if g != "}":
+            g = g.replace('."}]}}', "")
             print("Баланс: " + g)
 
         time.sleep(5)
-        vk.messages.send(peer_id=-166948584, random_id=randint(0, 999999999999), message="работа", payload="{\"menu\":\"jobs\"}")
+        requests.get("https://api.vk.com/method/messages.send?access_token=" + token + "&peer_id=-166948584&random_id=" + str(randint(0,99999999999)) + '&message=работа&payload={\"menu\":\"jobs\"}&v=5.131')
         time.sleep(5)
-        vk.messages.send(peer_id=-166948584, random_id=randint(0, 999999999999), message="🚖 таксист", payload="{\"jobs.menu\":\"taxi\"}")
+        requests.get("https://api.vk.com/method/messages.send?access_token=" + token + "&peer_id=-166948584&random_id=" + str(randint(0,99999999999)) + '&message=🚖 таксист&payload={\"jobs.menu\":\"taxi\"}&v=5.131')
         time.sleep(5)
-        g = vk.messages.send(peer_id=-166948584, random_id=randint(0, 999999999999), message="отправиться в путь", payload="{\"taxi1\":\"W19jYA==\"}")
-        str(g).replace("	", "")
+        g = requests.get("https://api.vk.com/method/messages.send?access_token=" + token + "&peer_id=-166948584&random_id=" + str(randint(0,99999999999)) + '&message=отправиться в путь&payload={\"taxi1\":\"W19jYA==\"}&v=5.131')
+        g = g.text
+        g = g.replace("{", "")
+        g = g.replace('response":', "")
+        g = g.replace("}", "")
+        g = g.replace('"', "")
         g = int(g) + 1
         time.sleep(5)
-        p =  vk.messages.getById(message_ids=g)
-        p = str(p)
+        p = requests.get("https://api.vk.com/method/messages.getById?access_token=" + token + "&message_ids=" + str(g) + "&v=5.131")
+        p = p.text
         time.sleep(5)
         if p.find("TAX4") != -1:
-            g = vk.messages.send(peer_id=-166948584, random_id=randint(0, 999999999999), message="🔼", payload="{\"taxi.begin1\":1,\"if\":1,\"city\":3}")
-            str(g).replace("	", "")
+            g = requests.get("https://api.vk.com/method/messages.send?access_token=" + token + "&peer_id=-166948584&random_id=" + str(randint(0,99999999999)) + '&message=🔼&payload={\"taxi.begin1\":1,\"if\":1,\"city\":3}&v=5.131')
+            g = g.text
+            g = g.replace("{", "")
+            g = g.replace('response":', "")
+            g = g.replace("}", "")
+            g = g.replace('"', "")
             g = int(g) + 1
             time.sleep(5)
-            p = vk.messages.getById(message_ids=g)
-            p = str(p)
+            p = requests.get("https://api.vk.com/method/messages.getById/?access_token=" + token + "&message_ids=" + str(g) + "&v=5.131")
+            p = p.text
             if p.find("35 сек") != -1:
                 print("В пути. Ждём 35 сек")
                 time.sleep(38)
@@ -52,14 +63,20 @@ while 1 == 1:
             else:
                 print("В пути. Ждём 52 секунды")
                 time.sleep(55)
-            vk.messages.send(peer_id=-166948584, random_id=randint(0, 999999999999), message="обновить время", payload="{\"taxi.road\":\"upd\"}")
+            requests.get("https://api.vk.com/method/messages.send?access_token=" + token + "&peer_id=-166948584&random_id=" + str(randint(0,99999999999)) + '&message=обновить время&payload={\"taxi.road\":\"upd\"}&v=5.131')
+
+
         elif p.find("TAX8") != -1:
-            g = vk.messages.send(peer_id=-166948584, random_id=randint(0, 999999999999), message="🔼", payload="{\"taxi.begin1\":2,\"if\":1,\"city\":3}")
-            str(g).replace("	", "")
+            g = requests.get("https://api.vk.com/method/messages.send?access_token=" + token + "&peer_id=-166948584&random_id=" + str(randint(0,99999999999)) + '&message=🔼&payload={\"taxi.begin1\":1,\"if\":1,\"city\":3}&v=5.131')
+            g = g.text
+            g = g.replace("{", "")
+            g = g.replace('response":', "")
+            g = g.replace("}", "")
+            g = g.replace('"', "")
             g = int(g) + 1
             time.sleep(5)
-            p = vk.messages.getById(message_ids=g)
-            p = str(p)
+            p = requests.get("https://api.vk.com/method/messages.getById/?access_token=" + token + "&message_ids=" + str(g) + "&v=5.131")
+            p = p.text
             if p.find("35 сек") != -1:
                 print("В пути. Ждём 35 сек")
                 time.sleep(38)
@@ -69,15 +86,19 @@ while 1 == 1:
             else:
                 print("В пути. Ждём 52 секунды")
                 time.sleep(55)
-            vk.messages.send(peer_id=-166948584, random_id=randint(0, 999999999999), message="обновить время", payload="{\"taxi.road\":\"upd\"}")
+            requests.get("https://api.vk.com/method/messages.send?access_token=" + token + "&peer_id=-166948584&random_id=" + str(randint(0,99999999999)) + '&message=обновить время&payload={\"taxi.road\":\"upd\"}&v=5.131')
 
         elif p.find("TAX5") != -1:
-            g = vk.messages.send(peer_id=-166948584, random_id=randint(0, 999999999999), message="🔼", payload="{\"taxi.begin1\":3,\"if\":1,\"city\":3}")
-            str(g).replace("	", "")
+            g = requests.get("https://api.vk.com/method/messages.send?access_token=" + token + "&peer_id=-166948584&random_id=" + str(randint(0,99999999999)) + '&message=🔼&payload={\"taxi.begin1\":1,\"if\":1,\"city\":3}&v=5.131')
+            g = g.text
+            g = g.replace("{", "")
+            g = g.replace('response":', "")
+            g = g.replace("}", "")
+            g = g.replace('"', "")
             g = int(g) + 1
             time.sleep(5)
-            p = vk.messages.getById(message_ids=g)
-            p = str(p)
+            p = requests.get("https://api.vk.com/method/messages.getById/?access_token=" + token + "&message_ids=" + str(g) + "&v=5.131")
+            p = p.text
             if p.find("35 сек") != -1:
                 print("В пути. Ждём 35 сек")
                 time.sleep(38)
@@ -87,15 +108,10 @@ while 1 == 1:
             else:
                 print("В пути. Ждём 52 секунды")
                 time.sleep(55)
-            vk.messages.send(peer_id=-166948584, random_id=randint(0, 999999999999), message="обновить время", payload="{\"taxi.road\":\"upd\"}")
+            requests.get("https://api.vk.com/method/messages.send?access_token=" + token + "&peer_id=-166948584&random_id=" + str(randint(0,99999999999)) + '&message=обновить время&payload={\"taxi.road\":\"upd\"}&v=5.131')
     except requests.exceptions.ConnectionError:
-        print("интернет атключили")
+        print("интернет отключили")
         time.sleep(5)
     except requests.exceptions.ReadTimeout:
-        print("интернет атключили")
-        time.sleep(5)
-    except vk_api.exceptions.Captcha:
-        print("НУЖНА КАПЧА! Ждем 30 сек.")
-        time.sleep(30)
-    except vk_api.exceptions.ApiError:
+        print("интернет отключили")
         time.sleep(5)
